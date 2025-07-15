485f36286dd3e99fa026ccec0c81c37db619aca2
Parith <meqtechemp25@gmail.com>
2025-07-15 19:27:27 +0530

    vercel.json file added and tailwind newversion added 4.1.4 to 4.1.11

--- vite.config.js
+++ vite.config.js
@@ -14,3 +14,18 @@ export default defineConfig({
     },
   },
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+// })
+
