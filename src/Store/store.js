aea63e624bcaad2e3c747f3c246d1bf1eb574c3d
Parith <meqtechemp25@gmail.com>
2025-07-16 18:11:43 +0530

    themes updated code

--- src/Store/store.js
+++ src/Store/store.js
@@ -27,7 +27,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["theme", "Auth" , "groups"], // only persist the theme slice
+  whitelist: ["Auth" , "groups"], // only persist the theme slice
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
